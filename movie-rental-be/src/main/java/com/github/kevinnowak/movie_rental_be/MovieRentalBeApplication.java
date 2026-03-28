ffa9530ac5e0183fb5b3779b88dfdfbbd9fb7817
Kevin Nowak <kevin.nowak@studium.fernuni-hagen.de>
2026-03-29 00:19:09 +0100

    Initial commit

---
+++ movie-rental-be/src/main/java/com/github/kevinnowak/movie_rental_be/MovieRentalBeApplication.java
@@ -0,0 +1,13 @@
+package com.github.kevinnowak.movie_rental_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRentalBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRentalBeApplication.class, args);
+	}
+
+}
---
+++ movie-rental-be/src/test/java/com/github/kevinnowak/movie_rental_be/MovieRentalBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kevinnowak.movie_rental_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRentalBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
